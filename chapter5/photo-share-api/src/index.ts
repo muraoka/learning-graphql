b1fc3834708d37dbc2a78356b881619ec03eb364
muraoka <taigamuraoka1998@gmail.com>
2020-06-22 09:29:54 +0900

    Add chapter 5-4

--- chapter5/photo-share-api/src/index.ts
+++ chapter5/photo-share-api/src/index.ts
@@ -2,19 +2,34 @@ import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
 import expressPlayGround from 'graphql-playground-middleware-express';
 import { readFileSync } from 'fs';
+import MongoClient from 'mongodb';
+import dotenv from 'dotenv';
 import resolvers from './resolvers';
 
+dotenv.config();
 const typeDefs = readFileSync('././typeDefs.graphql', 'utf8');
 
-const app = express();
-const server = new ApolloServer({ typeDefs, resolvers });
-server.applyMiddleware({ app });
+async function start() {
+  const app = express();
 
-app.get('/', (req, res) => res.end('Welcome to the PhotoShare API'));
-app.get('/playground', expressPlayGround({ endpoint: '/graphql' }));
+  const MONGO_DB = process.env.DB_HOST;
+  const client = await MongoClient.connect(MONGO_DB, { useNewUrlParser: true });
+  const db = client.db();
 
-app.listen({ port: 4000 }, () =>
-  console.log(
-    `GraphQL Server running @ http://localhost:4000${server.graphqlPath}`
-  )
-);
+  const context = { db };
+
+  const server = new ApolloServer({ typeDefs, resolvers, context });
+
+  server.applyMiddleware({ app });
+
+  app.get('/', (req, res) => res.end('Welcome to the PhotoShare API'));
+  app.get('/playground', expressPlayGround({ endpoint: '/graphql' }));
+
+  app.listen({ port: 4000 }, () =>
+    console.log(
+      `GraphQL Server running @ http://localhost:4000${server.graphqlPath}`
+    )
+  );
+}
+
+start();
