ab19b98b12d886f781290751ef66d9a253f21367
muraoka <taigamuraoka1998@gmail.com>
2020-06-25 10:36:31 +0900

    Add chapter 6-3-2

--- chapter6/photo-share-client/src/index.js
+++ chapter6/photo-share-client/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ApolloProvider } from 'react-apollo'
+import ApolloClient from 'apollo-boost'
+
+const client = new ApolloClient({ uri: 'http://localhost:4000/graphql' })
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
